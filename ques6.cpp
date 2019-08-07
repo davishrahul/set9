8de8a0998b27112ea1d324246010610abb31a4bc
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-08-07 07:07:21 +0530

    Create ques6.cpp

---
+++ ques6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string input;//123
+    int count=0;
+    cin>>input;
+    for(int i=0;i<input.length();i++)
+    {
+        if(input[i]==input[i+1])
+        {
+            count++;
+        }
+    }
+    if(count>0)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+    
+}
