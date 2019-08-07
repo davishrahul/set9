42958a152dc0e8c3c970d113617e7eaf16e8bc30
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-08-07 06:39:46 +0530

    Create ques2.cpp

---
+++ ques2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main ()
+{
+   float length,breadth,result;
+   cin>>length>>breadth;
+   result=length*breadth;
+   cout<<fixed<<setprecision(5)<<result<<endl;
+
+}
