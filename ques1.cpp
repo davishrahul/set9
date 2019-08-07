ed7a41d32dfd1e0336530dcd711f91c50ad2d0a1
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-08-07 06:33:01 +0530

    Create ques1.cpp

---
+++ ques1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int number1,number2,result;
+    while(!cin.eof())
+    {
+        cin>>number1>>number2;
+        result=number2-number1;
+        cout<<result<<endl;
+            }
+}
